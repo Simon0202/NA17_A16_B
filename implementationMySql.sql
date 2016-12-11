a55056cae2d2b61a0abfc7e2106baf9a9787aa11
Sam <souv.kun@gmail.com>
2016-12-11 05:44:09 +0100

    Correction des erreurs : implémentation fonctionnelle

--- implementationMySql.sql
+++ implementationMySql.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE Utilisateur(
 email VARCHAR(50) PRIMARY KEY,
 nom VARCHAR(20) NOT NULL,
@@ -14,27 +13,28 @@ metier VARCHAR(20)
 CREATE TABLE Flux(
 titre VARCHAR(50) PRIMARY KEY,
 confidentialite VARCHAR(20) CHECK (confidentialite = 'public' OR confidentialite = 'prive' OR confidentialite ='restreint'),
+createur VARCHAR(50) NOT NULL REFERENCES Utilisateur(email)
 );
 
 CREATE TABLE Groupe_Lecteurs(
-id_lecteur INTEGER(10) PRIMARY KEY,
+id_lecteur INT PRIMARY KEY,
 email_admin VARCHAR(50) NOT NULL REFERENCES Utilisateur(email),
-id_publi INTEGER NOT NULL REFERENCES Flux(id_publi)
+titre VARCHAR(50) NOT NULL REFERENCES Flux(titre)
 );
 
 CREATE TABLE Groupe_Redacteurs(
-id_lecteur INTEGER(10) PRIMARY KEY REFERENCES Groupe_Lecteurs(id_lecteur),
-id_redacteur INTEGER(10) UNIQUE
+id_lecteur INT PRIMARY KEY REFERENCES Groupe_Lecteurs(id_lecteur),
+id_redacteur INT UNIQUE
 );
 
 CREATE TABLE Publication(
 lien VARCHAR(50) PRIMARY KEY,
-id_lecteur INTEGER(10) NOT NULL REFERENCES Groupe_Lecteurs(id_lecteur),
-id_redacteur INTEGER(10) NOT NULL REFERENCES Groupe_Redacteurs(id_redacteur),
+id_lecteur INT NOT NULL REFERENCES Groupe_Lecteurs(id_lecteur),
+id_redacteur INT NOT NULL REFERENCES Groupe_Redacteurs(id_redacteur),
 titre VARCHAR(50) NOT NULL,
-date_publi DATETIME NOT NULL,
-etat VARCHAR(20) CHECK (etat = 'vailde' OR etat = 'rejete' OR etat = 'supprime'),
-)
+date_publi DATE NOT NULL,
+etat VARCHAR(20) CHECK (etat = 'valide' OR etat = 'rejete' OR etat = 'supprime')
+);
 
 CREATE TABLE Article(
 lien VARCHAR(50) PRIMARY KEY REFERENCES Publication(lien),
@@ -51,15 +51,15 @@ url VARCHAR(50)
 CREATE TABLE Lire(
 lien_publi VARCHAR(50) REFERENCES Publication(lien),
 email VARCHAR(50) REFERENCES Utilisateur(email),
-id_lecteur INTEGER(10) REFERENCES Groupe_Lecteurs(id_lecteur),
-PRIMARY KEY (id_publi, email),
+id_lecteur INT REFERENCES Groupe_Lecteurs(id_lecteur),
+PRIMARY KEY (lien_publi, email),
 vote VARCHAR(10) CHECK (vote='like' OR vote='dislike' OR vote='null'),
 commentaire VARCHAR(450)
 );
 
 CREATE TABLE Compo_Groupe(
 email VARCHAR(50) REFERENCES Utilisateur(email),
-id_lecteur INTEGER(10) REFERENCES Groupe_Lecteurs(id_lecteur),
+id_lecteur INT REFERENCES Groupe_Lecteurs(id_lecteur),
 PRIMARY KEY(email, id_lecteur)
 );
 
@@ -67,36 +67,36 @@ INSERT INTO Utilisateur (email, nom, prenom, entreprise)
 VALUES ('michel.durand@email.com','Durand','Michel','Big Company'),	-- Responsables de groupes
  ('micheld@email.com','Durent','Michel','Big Company'),
  ('paul@email.com','Gerdon','Paul','Small Company'),
- ('fançois@email.com','Klezt','François','Small Company')
+ ('fançois@email.com','Klezt','François','Small Company'),
  
- ('Inconnu@email.com','n°1','Inconnu','Small Company')		-- éléments de tests pour les appartenances de groupe
- ('Inconnu2@email.com','n°2','Inconnu','Small Company')
- ('Inconnu3@email.com','n°3','Inconnu','Small Company')
- ('Inconnu4@email.com','n°4','Inconnu','Small Company')
- ('Inconnu5@email.com','n°5','Inconnu','Small Company')
- ('Inconnu6@email.com','n°6','Inconnu','Small Company')
- ('Inconnu7@email.com','n°7','Inconnu','Small Company')
- ('Inconnu8@email.com','n°8','Inconnu','Small Company')
- ('Inconnu9@email.com','n°9','Inconnu','Small Company')
- ('Inconnu10@email.com','n°10','Inconnu','Small Company')
- ('Inconnu11@email.com','n°11','Inconnu','Small Company')
+ ('inconnu@email.com','n°1','inconnu','Small Company'),	-- éléments de tests pour les appartenances de groupe
+ ('inconnu2@email.com','n°2','inconnu','Small Company'),
+ ('inconnu3@email.com','n°3','inconnu','Small Company'),
+ ('inconnu4@email.com','n°4','inconnu','Small Company'),
+ ('inconnu5@email.com','n°5','inconnu','Small Company'),
+ ('inconnu6@email.com','n°6','inconnu','Small Company'),
+ ('inconnu7@email.com','n°7','inconnu','Small Company'),
+ ('inconnu8@email.com','n°8','inconnu','Small Company'),
+ ('inconnu9@email.com','n°9','inconnu','Small Company'),
+ ('inconnu10@email.com','n°10','inconnu','Small Company'),
+ ('inconnu11@email.com','n°11','inconnu','Small Company')
  ;
 
 INSERT INTO Flux
-VALUES (1,'Avions et création','public','michel.durand@email.com'),
-(2,'BDD SQL','public','michel.durand@email.com'),
-(3,'Programmation objet','public','paul@email.com')
+VALUES ('Avions et création','public','michel.durand@email.com'),
+('BDD SQL','public','michel.durand@email.com'),
+('Programmation objet','public','paul@email.com')
 ;
 
 
 INSERT INTO Groupe_Lecteurs
-VALUES (1,'michel.durand@email.com',1),		-- 1er Groupe Lecteur du premier flux
-(2,'michel.durand@email.com',1),			-- 1er Groupe Redacteur du premier flux
-(3,'micheld@email.com',1),					-- 2ème Groupe Lecteur du premier flux
-(4,'michel.durand@email.com',2),			-- 1er Groupe Lecteur du second flux
-(5,'michel.durand@email.com',2)				-- 1er Groupe Redacteur du second flux
-(6,'michel.durand@email.com',3),			-- 1er Groupe Lecteur du troisième flux
-(7,'michel.durand@email.com',3)				-- 1er Groupe Redacteur du troisième flux
+VALUES (1,'michel.durand@email.com','Avions et création'),		-- 1er Groupe Lecteur du premier flux
+(2,'michel.durand@email.com','Avions et création'),			-- 1er Groupe Redacteur du premier flux
+(3,'micheld@email.com','Avions et création'),					-- 2ème Groupe Lecteur du premier flux
+(4,'michel.durand@email.com','BDD SQL'),			-- 1er Groupe Lecteur du second flux
+(5,'michel.durand@email.com','BDD SQL'),				-- 1er Groupe Redacteur du second flux
+(6,'michel.durand@email.com','Programmation objet'),			-- 1er Groupe Lecteur du troisième flux
+(7,'michel.durand@email.com','Programmation objet')				-- 1er Groupe Redacteur du troisième flux
 ;
 
 INSERT INTO Groupe_Redacteurs
@@ -113,7 +113,7 @@ VALUES
 	('inconnu3@email.com',1),
 ('michel.durand@email.com',2),
 	('inconnu4@email.com',1),
-('michel.d@email.com',3),
+('micheld@email.com',3),
 	('inconnu4@email.com',3),
 ('michel.durand@email.com',4),
 	('inconnu5@email.com',4),
@@ -126,12 +126,12 @@ VALUES
 
 INSERT INTO Publication
 VALUES
-('www.avions/article.com',1,1,'Vieux avions',01012016,'valide'),
-('www.avions/article2.com',2,1,'Nouveaux avions',01012016,'valide'),
-('www.bddpourlavie/extrait.com',4,2,'Le SQL',01012016,'valide'),
-('www.mauvaissite/extrait.com',5,2,'Langages pour BDD',01012016,'rejete'),
-('www.cplusplus.com',6,3,'Le C++',01012016,'valide'),
-('www.cplusplus/videos.com',7,3,'Le C++',01012016,'valide')
+('www.avions/article.com',1,1,'Vieux avions','20160101','valide'),
+('www.avions/article2.com',2,1,'Nouveaux avions','20160101','valide'),
+('www.bddpourlavie/extrait.com',4,2,'Le SQL','20160101','valide'),
+('www.mauvaissite/extrait.com',5,2,'Langages pour BDD','20160101','rejete'),
+('www.cplusplus.com',6,3,'Le C++','20160101','valide'),
+('www.cplusplus/videos.com',7,3,'Le C++','20160101','valide')
 ;
 
 INSERT INTO Article
@@ -145,7 +145,7 @@ VALUES
 
 INSERT INTO Multimedia
 VALUES
-('www.cplusplus/videos.com''Designs patterns',NULL)
+('www.cplusplus/videos.com','Designs patterns',NULL)
 ;
 
 INSERT INTO Lire

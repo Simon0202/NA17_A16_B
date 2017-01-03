8191f5a282f43219cf008712f212e1e69d18e272
楼杭欣 <hangxin.lou@gmail.com>
2017-01-03 13:45:21 +0100

    Ajout de l'affichage de groupes dans homePage.php

--- homePage.php
+++ homePage.php
@@ -35,22 +35,49 @@
         ?>
     </div>
     <div id="groupeWidget">
-      <h2>Groupes auxquels vous appartenez</h2>
-    </div>
     <?php
-
-
-      $result = mysqli_query($link, "INSERT INTO Utilisateur (email, nom, prenom, entreprise, genre, pays, metier) VALUES ('$emailUtilisateur','$nomUtilisateur', '$prenomUtilisateur', '$entrepriseUtilisateur', '$genreUtilisateur', '$paysUtilisateur','$metierUtilisateur');");
-
-      if (! $fetch =mysqli_fetch_row($result)) {
-        echo "<div>Aucun enregistrement ne correspond\n</div>";
-      }
-      else {
-        echo"<tr>$fetch[0] $fetch[1] $fetch[2] $fetch[3] $fetch[4] $fetch[5] </tr>";
-      } 
-      pg_close(); 
+        require_once ('connect.php');
+        $result = pg_query($bddconn, "SELECT email_admin, nom FROM groupe_utilisateur WHERE groupe_utilisateur.email_admin='$mailSession';");
+        $row = pg_fetch_row($result);
+        if($row[0] !== null){
+            echo "<h2>Informations du groupe : </h2>";
+            echo"<br/>";
+            echo "<div>Nom du groupe : $row[1] </div>";
+            echo "<div>E-mail de responsable du groupe : $mailSession</div>";
+            echo "<div>List de membre du group : </div>";
+            $a=$row[1];
+            $result = pg_query($bddconn, "SELECT u.nom, u.prenom FROM compo_groupe cg, utilisateur u WHERE u.email= cg.email and cg.nom='$a';");
+            $row = pg_fetch_row($result);
+            if($row[0] == null){
+                echo "<div>Vous n'avez aucun membre dans ce groupe : </div>";
+            }else{
+                echo "<div>                  $row[1], $row[0] </div>";
+                while($row = pg_fetch_row($result)){
+                    echo "<div>                  $row[1], $row[0] </div>";
+                }
+            }
+        }else{
+            $result = pg_query($bddconn, "SELECT nom FROM compo_groupe WHERE compo_groupe.email='$mailSession';");
+            $row = pg_fetch_row($result);
+            if($row[0] == null){
+                echo "<h2>Vous n'appartenez aucun groupe : </h2>";
+            }else{
+                $nom_g=$row[0];
+                $result = pg_query($bddconn, "SELECT gu.email_admin, u.nom, u.prenom FROM compo_groupe cg, utilisateur u, groupe_utilisateur gu WHERE u.email= cg.email and cg.nom = gu.nom and cg.nom='$nom_g';");
+                $row = pg_fetch_row($result);
+                echo "<h2>Informations du groupe : </h2>";
+                echo"<br/>";
+                echo "<div>Nom du groupe : $nom_g </div>";
+                echo "<div>E-mail de responsable du groupe : $row[0]</div>";
+                echo "<div>List de membre du group : </div>";
+                echo "<div>$row[2], $row[1] </div>";
+                while($row = pg_fetch_row($result)){
+                    echo "<div>$row[2], $row[1] </div>";
+                }
+            }
+        }
     ?>
-	<br/>
+    </div>
     <a href="index.php"><button>Deconnexion</button></a>
   </body>
 </html>
